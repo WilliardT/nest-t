4f7eb2ceaf9245ec60e47e0b66bcedcf6878658d
g.titov <g.titov@mastercr.ru>
2026-03-08 16:21:15 +0300

    TypeOrmModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,23 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from "@nestjs/typeorm";
 
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres', // тип бвзы данных
+      host: 'localhost', //  хост бд
+      port: 5432, // стандартный порт PostgreSQL
+      username: 'postgres_user', // имя пользователя в бд
+      password: 'postgres_pass', // пароль от бд
+      database: 'nestjs_bd', // имя базы данных
+      autoLoadEntities: true, // автоматически подключает все entity
+      synchronize: true // авто создание таблиц по entity
+    }),
+    UsersModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
