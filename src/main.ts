a7a00fb5ed5dd8af8f5c39b9b2f3de0769f38ef6
g.titov <g.titov@mastercr.ru>
2026-03-09 09:25:09 +0300

    app.enableCors

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,13 @@ async function bootstrap() {
   // валидация для DTO
   app.useGlobalPipes(new ValidationPipe());
 
+  // app.enableCors({
+  //   origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
+  //   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  //   allowedHeaders: ['Content-Type', 'Authorization'],
+  //   credentials: false // куки или сессии
+  // })
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
