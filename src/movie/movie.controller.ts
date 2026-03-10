5d14c7d4057926b035e755b96b01e7093d3428aa
g.titov <g.titov@mastercr.ru>
2026-03-10 08:17:42 +0300

    MovieModule Controller

--- src/movie/movie.controller.ts
+++ src/movie/movie.controller.ts
@@ -1,10 +1,56 @@
-import { Controller } from '@nestjs/common';
+import {Body, Controller, Get, Param, Post, Query, Req, Res} from '@nestjs/common';
 import { MovieService } from './movie.service';
 
 @Controller({
   path: 'movies'
-  // etc settings if need
+  // etc settings
 })
 export class MovieController {
   constructor(private readonly movieService: MovieService) {}
+
+  @Get()
+  findAll(@Query() query: any){
+    return {}
+  }
+
+  @Get(':id')
+  findById(@Param('id') id:string){
+    return { id }
+  }
+
+  @Post()
+  create(@Body() body: { title: string }) {
+    return body
+  }
+
+  // заголовки
+  // @Get('headers')
+  // getHeaders(@Headers() headers: any) {
+  //   return headers
+  // }
+  //
+  // // c какого приложения был отправлен
+  // @Get('user-agent')
+  // getHeaders(@Headers('user-agent') userAgent: string) {
+  //   return { userAgent }
+  // }
+  //
+  // // с объектом запроса
+  // @Get()
+  // getRequestDetails(@Req() req: Request) {
+  //   return {
+  //     method: req.method,
+  //     url: req.url,
+  //     headers: req.headers
+  //   }
+  // }
+  //
+  // // с объектом ответа
+  // @Get()
+  // getRequestDetails(@Res() res: Response) { // @Ip @Session получить ip и сессию
+  //   res.status(201)
+  //     .json({ message: 'Hello' })
+  // }
+
 }
+
