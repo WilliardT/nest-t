8c7e78e8897075d2df15e73374241a4c1c77caa3
g.titov <g.titov@mastercr.ru>
2026-03-12 17:08:11 +0300

    ResponseInterceptor

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ResponseInterceptor } from "./common/interceptors/response.interceptor";
 
 
 async function bootstrap() {
@@ -16,6 +17,8 @@ async function bootstrap() {
     stopAtFirstError: true
   }));
 
+
+  app.useGlobalInterceptors(new ResponseInterceptor()) // для преобразования данных
 
   // app.enableCors({
   //   origin: ['http://localhost:3001', 'http://127.0.0.1:3001'],
