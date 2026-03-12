69302789fb7f61c7e412f5ba5c179f3e58585961
g.titov <g.titov@mastercr.ru>
2026-03-12 18:36:50 +0300

    SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { ResponseInterceptor } from "./common/interceptors/response.interceptor";
 import { AllExceptionFilter } from "./common/filters/all-exceptions.filter";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 
 
 async function bootstrap() {
@@ -32,6 +33,18 @@ async function bootstrap() {
 
 
   //app.setGlobalPrefix('api')
+
+  // todo вынести
+  const config = new DocumentBuilder()
+    .setTitle('Documentation')
+    .setDescription('API documentation:')
+    .setVersion('1.0.0')
+    .build()
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/docs', app, document);
+  // <==
 
   await app.listen(3000);
 }
