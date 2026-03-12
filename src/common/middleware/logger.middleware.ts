039d37469d3a4d68f4e627ee4ca79c818a2277ae
g.titov <g.titov@mastercr.ru>
2026-03-12 15:01:12 +0300

    LoggerMiddleware 2

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -1,12 +1,17 @@
 import { Injectable, NestMiddleware } from "@nestjs/common";
+import type { Request, Response, NextFunction } from "express";
 
 
 @Injectable()
 export class LoggerMiddleware implements NestMiddleware {
-  use(req: any, res: any, next:(error?: any) => void) {
+  use(
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) {
     const fullUrl = `[${req.protocol}]://${req.get("host")}${req.originalUrl}`;
 
-    console.log(`[${req.method}] ${fullUrl}`);
+    console.log(`Request: [${req.method}] ${fullUrl}`);
 
     next();
   }
