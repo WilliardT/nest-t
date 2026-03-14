3c08e4d3ff0e941b73e2f50c668cb18254001380
g.titov <g.titov@mastercr.ru>
2026-03-14 04:44:42 +0300

    hash(password) argon2

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -4,6 +4,7 @@ import { UserEntity } from "./entities/user.entity";
 import { QueryFailedError, Repository } from "typeorm";
 import { RegisterRequest } from "./dto/register.dto";
 import { PgErrorCode } from "../common/constants/pg-error-codes";
+import { hash } from "argon2";
 
 
 @Injectable()
@@ -17,7 +18,11 @@ export class AuthService {
     const { name, email, password } = dto
 
     try {
-      const user: UserEntity = this.userRepository.create({ name, email, password })
+      const user: UserEntity = this.userRepository.create({
+        name,
+        email,
+        password: await hash(password)
+      })
 
       return await this.userRepository.save(user)
 
