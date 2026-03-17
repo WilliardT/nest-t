e06d72f7f147c4d6ff5ba1d871ec93577bb851f1
williardt <609353@bk.ru>
2026-03-17 08:25:49 +0300

    CustomLoggerService with time

--- src/common/middleware/logger.middleware.ts
+++ src/common/middleware/logger.middleware.ts
@@ -14,9 +14,19 @@ export class LoggerMiddleware implements NestMiddleware {
     res: Response,
     next: NextFunction
   ) {
+    //const startedAt = process.hrtime.bigint();
     const fullUrl = `[${req.protocol}]://${req.get("host")}${req.originalUrl}`;
 
     this.customLoggerService.log(`Request: [${req.method}] ${fullUrl}`, LoggerMiddleware.name);
+
+    // res.on("finish", () => {
+    //   const durationMs = Number(process.hrtime.bigint() - startedAt) / 1_000_000;
+    //
+    //   this.customLoggerService.log(
+    //     `Response: [${req.method}] ${fullUrl} ${res.statusCode} ${durationMs.toFixed(2)}ms`,
+    //     LoggerMiddleware.name
+    //   );
+    // });
 
     next();
   }
